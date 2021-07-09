1c80cd0f1a3209189514869a2039165b4748bd37
Dr. Konstantin Shmirko <kshmirko@gmail.com>
2021-07-10 01:35:37 +1000

    123123

--- data.asm
+++ data.asm
@@ -1,6 +1,6 @@
 .cseg
 ;.org		(FLASHEND-400)
-buildtime:	.db "RELEASE DATE %DAY%.%MONTH%.%YEAR% %HOUR%:%MINUTE%",0
+buildtime:	.db "RELEASE DATE %YEAR%%MONTH%%DAY%%HOUR%%MINUTE%",0
 ;header:		.db "This program is Probnik-04 firmware",10,13, 0
 ;author:		.db "Ph.D. Shmirko K.A. ",10,13,0
 
